591cea8b3d6da73dcb41cf9d225a641f87f06cf8
Buggem's Fedora PC <66670928+Buggem@users.noreply.github.com>
2025-01-30 14:53:41 +1100

    Fix speed

--- source/donut.h
+++ source/donut.h
@@ -6,16 +6,18 @@
 
 #if __3DS__
 
-#define DELAY 1000
+#define DELAY 0
+#define SPEED_A 0.00004
+#define SPEED_B 0.00002
+
 
 #else
 
 #define DELAY 60000
-
-#endif
-
 #define SPEED_A 0.00004
 #define SPEED_B 0.00002
+
+#endif
 
 #define FF_MULTI 4
 
